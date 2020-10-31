c4f36f43f395ac57ca04aa30c6a11818a42a1135
daveT83 <40746034+daveT83@users.noreply.github.com>
2020-10-31 01:42:10 -0400

    Add files via upload

---
+++ InternetSpeedLogger/Program.cs
@@ -0,0 +1,24 @@
+﻿using InternetSpeedLogger.Infrastructure;
+using InternetSpeedLogger.Models;
+using System;
+using System.Configuration;
+
+namespace InternetSpeedLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dtNow = DateTime.Now;
+            Logger logger = new Logger(ConfigurationManager.AppSettings["LogFolder"]+@"InternetMonitorLog_" + dtNow.ToString("MM-dd-yyyy") + ".txt");
+            logger.Log("Begining Log: " + dtNow.Hour + ":" + dtNow.Minute + ":" + dtNow.Second);
+            logger.Log("______________________________________________________________________________________________________");
+
+            SpeedTester st = new SpeedTester(logger, ConfigurationManager.AppSettings["ResultsFolder"]+@"InternetMonitorResults_" + dtNow.ToString("MM-dd-yyyy") + ".csv", dtNow);
+            Download download = st.TestInternet();
+
+            logger.Log("End of Log");
+            logger.Log("______________________________________________________________________________________________________");
+        }
+    }
+}
